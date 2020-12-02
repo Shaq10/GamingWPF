43c40f19c03c03ad6710d2e8b8365da3948a2198
Shaq10 <shaquille096@gmail.com>
2020-12-02 03:20:49 +0000

    Updated README, images added, created CRUDManager

--- GamingWPFBusiness/CRUDManager.cs
+++ GamingWPFBusiness/CRUDManager.cs
@@ -1,14 +1,128 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using GamingWPF;
 
 namespace GamingWPFBusiness
 {
-    class CRUDManager
+    public class CRUDManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
+
+        // Selecting Entities
+        public GamingWPF.Console SelectedConsole { get; set; }
+        public Genre SelectedGenre { get; set; }
+        public Game SelectedGame { get; set; }
+        public Customer SelectedCustomer { get; set; }
+        public Order SelectedOrder { get; set; }
+
+        public void SetSelectedConsole(object selectedConsole) {
+            SelectedConsole = (GamingWPF.Console)selectedConsole;
+        }
+
+        public void SetSelectedGenre(object selectedGenre)
+        {
+            SelectedGenre = (Genre)selectedGenre;
+        }
+
+        public void SetSelectedGame(object selectedGame)
+        {
+            SelectedGame = (Game)selectedGame;
+        }
+
+        public void SetSelectedCustomer(object selectedCustomer)
+        {
+            SelectedCustomer = (Customer)selectedCustomer;
+        }
+
+        public void SetSelectedOrder(object selectedOrder)
+        {
+            SelectedOrder = (Order)selectedOrder;
+        }
+
+        // Retrieving Entities
+
+        public List<GamingWPF.Console> RetrieveAllConsoles() {
+            using (var db = new GamingContext()) {
+                return db.Consoles.ToList();
+            }
+        }
+        public List<Genre> RetrieveAllGenres()
+        {
+            using (var db = new GamingContext())
+            {
+                return db.Genres.ToList();
+            }
+        }
+        public List<Game> RetrieveAllGames()
+        {
+            using (var db = new GamingContext())
+            {
+                return db.Games.ToList();
+            }
+        }
+
+        public List<Customer> RetrieveAllCustomers()
+        {
+            using (var db = new GamingContext())
+            {
+                return db.Customers.ToList();
+            }
+        }
+        public List<Order> RetrieveAllOrders()
+        {
+            using (var db = new GamingContext())
+            {
+                return db.Orders.ToList();
+            }
+        }
+
+        // Creating Entities
+
+        public void CreateConsole(string consoleName, string manufacturer, char online) {
+            using (var db = new GamingContext()) {
+                var newConsole = new GamingWPF.Console() {
+                    ConsoleName = consoleName.Trim(),
+                    Manufacturer = manufacturer.Trim(),
+                    OnlineCompatible = online.ToString()
+                };
+            }
+        }
+
+        public void CreateGenre(string genreName)
+        {
+            using (var db = new GamingContext())
+            {
+                var newGenre = new Genre()
+                {
+                    GenreName = genreName.Trim()
+                };
+            }
+        }
+
+        public void CreateGame(string gameID, int consoleID, int genreID, string title, int age, 
+            decimal price, string publisher, DateTime release, int multi) {
+            using (var db = new GamingContext()) {
+                var newGame = new Game()
+                {
+                    GameId = gameID.Trim(),
+                    ConsoleId = consoleID,
+                    GenreId = genreID,
+                    Title = title.Trim(),
+                    AgeRating = age,
+                    Price = price,
+                    Publisher = publisher.Trim(),
+                    ReleaseDate = release,
+                    Multiplayers = multi
+                };
+            }
+        }
+
+
+
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # GamingWPF
 Repo for the gaming WPF which I'm creating for my individual project
+
+# Entity Relationship Diagram
+................................
+
+# Class Diagram
+...............................
+
+## Project Goal
+The goal of this project is to create a WPF application which provides a game 
+purchasing service for customers. Customers can view games available on a number 
+of consoles and choose which to purchase. The project will include all the documentation 
+required to show the process of what was done, how and why
+
+# Sprints
+![image](C:/github/Eng71/IndividualProject/PreSprints.png)
+
+## Sprint 1 - 30/11/2020
+![image](C:/github/Eng71/IndividualProject/Sprint1.png)
+### Sprint Goal
+- [x] Build Database
+- [ ] Create business layer
+- [ ] Build initial GUI
+- [ ] Make a Create Game method
+- [x] Update README file
+- [x] Commit changes to Github
+- [ ] Complete Review
+- [ ] Complete Retrospective
+
+### Sprint Review
+
+
+### Sprint Retrospective
